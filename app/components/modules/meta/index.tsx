5d3a5eec84627ed998a1ea748560c900dae48278
Breyner <mon2bdr@gmail.com>
2021-04-27 19:23:22 -0500

    Fix: Remove unused imports

--- app/components/modules/meta/index.tsx
+++ app/components/modules/meta/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../../../lib/constants'
+import { HOME_OG_IMAGE_URL } from '../../../lib/constants'
 import { GA_TRACKING_ID } from "../../../lib/gtags";
 
 const Meta = ({title}: { title: string }) => {
