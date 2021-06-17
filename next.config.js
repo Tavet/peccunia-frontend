2118f45b336366dcdb6ea9ae0e3498a433782019
Breyner <mon2bdr@gmail.com>
2021-06-17 18:21:03 -0500

    Update next.config.js webpack5 flag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    future: {
-        webpack5: true,
-    },
+    webpack5: true,
     env: {
         API_BASE_URL: process.env.API_BASE_URL,
         API_TOKEN: process.env.API_TOKEN
